238b81e493f48755c6e04aed15c6e4f7b19441d5
EmilioQuinonez <150107914+EmilioQuinonez@users.noreply.github.com>
2023-11-29 20:05:43 -0600

    Update Contacto.cpp

--- Contacto.cpp
+++ Contacto.cpp
@@ -12,22 +12,28 @@ void Contacto::agregarEmail(string email) {
     this->email = email;
 }
 
-void Contacto::agregarDireccion(string calle, string ciudad, string codigoPostal, string estado, string pais) {
-    direccion.modificarDireccion(calle, ciudad, codigoPostal, estado, pais);
+void Contacto::agregarDatosDireccion(string calle, string ciudad, string codigoPostal, string estado, string pais) {
+    direccion.modificarCalle(calle);
+    direccion.modificarCiudad(ciudad);
+    direccion.modificarCodigoPostal(codigoPostal);
+    direccion.modificarEstado(estado);
+    direccion.modificarPais(pais);
 }
 
-void Contacto::agregarTelefono(string numero, string tipo, string empresa, string cargo) {
-    numeroTelefono.asignarNumeroCompleto(numero, tipo, empresa, cargo);
+void Contacto::agregarDatosTelefono(string numero, string tipo, string empresa, string cargo) {
+    numeroTelefono.asignarNumero(numero);
+    numeroTelefono.asignarTipo(tipo);
+    numeroTelefono.asignarEmpresa(empresa);
+    numeroTelefono.asignarCargo(cargo);
 }
 
 string Contacto::obtenerInformacionContacto() {
     string infoContacto = "Nombre: " + nombre + "\n";
     infoContacto += "Email: " + email + "\n";
-    infoContacto += "Dirección: " + direccion.obtenerDireccionCompleta() + "\n";
+    infoContacto += "Dirección: " + direccion.obtenerCalle() + direccion.obtenerCiudad() + direccion.obtenerCodigoPostal() + direccion.obtenerEstado() + direccion.obtenerPais() + "\n";
     infoContacto += "Teléfono: " + numeroTelefono.obtenerNumero() + " (" + numeroTelefono.obtenerTipo() + ")\n";
     infoContacto += "Empresa: " + numeroTelefono.obtenerEmpresa() + "\n";
     infoContacto += "Cargo: " + numeroTelefono.obtenerCargo() + "\n";
 
     return infoContacto;
 }
-

21461385b21aab27c53e3e2ae22194dbbb442659
EmilioQuinonez <150107914+EmilioQuinonez@users.noreply.github.com>
2023-11-29 20:42:12 -0600

    Update Contacto.h

--- Contacto.h
+++ Contacto.h
@@ -2,25 +2,26 @@
 #define CONTACTO_H
 
 #include <string>
-#include "Direccion.h"
-#include "NumeroTelefonoProfesional.h"
+#include "Direccion.h" // Incluimos la definición de la clase Direccion porque ocupamos elementos de esta clase.
+#include "NumeroTelefonoProfesional.h" // Incluimos la definición de la clase NumeroTelefonoProfesional porque ocupamos elementos de esta clase.
+
 
 using namespace std;
 
 class Contacto {
 private:
-    string nombre;
-    string email;
-    Direccion direccion;
-    NumeroTelefonoProfesional numeroTelefono;
+    string nombre; // Nombre del contacto.
+    string email; // Correo electrónico del contacto.
+    Direccion direccion; // Objeto Direccion que representa la dirección del contacto.
+    NumeroTelefonoProfesional numeroTelefono; // Objeto NumeroTelefonoProfesional que representa el número de teléfono del contacto.
 
 public:
-    Contacto();
-    void agregarNombre(string nombre);
-    void agregarEmail(string email);
-    void agregarDatosTelefono(string numero, string tipo, string empresa, string cargo);
-    void agregarDatosDireccion(string calle, string ciudad, string codigoPostal, string estado, string pais);
-    string obtenerInformacionContacto();
+    Contacto(); // Constructor de la clase Contacto
+    void agregarNombre(string nombre); // Método para establecer el nombre del contacto
+    void agregarEmail(string email); // Método para establecer el email del contacto
+    void agregarDatosTelefono(string numero, string tipo, string empresa, string cargo); // Método para establecer los datos del teléfono del contacto
+    void agregarDatosDireccion(string calle, string ciudad, string codigoPostal, string estado, string pais); // Método para establecer los datos de la dirección del contacto
+    string obtenerInformacionContacto(); // Método para obtener la información completa del contacto
 };
 
 #endif

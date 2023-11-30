84216eb50fb414da563087a8d982138d93230ce0
EmilioQuinonez <150107914+EmilioQuinonez@users.noreply.github.com>
2023-11-29 20:42:40 -0600

    Update Direccion.h

--- Direccion.h
+++ Direccion.h
@@ -7,24 +7,24 @@ using namespace std;
 
 class Direccion {
 private:
-    string calle;
-    string ciudad;
-    string codigoPostal;
-    string estado;
-    string pais;
+    string calle; // Nombre de la calle del objeto Direccion. 
+    string ciudad; // Nombre de la ciudad del objeto Direccion. 
+    string codigoPostal; // Nombre del codio postal del objeto Direccion. 
+    string estado; // Nombre del estado del objeto Direccion. 
+    string pais; // Nombre del pais del objeto Direccion. 
 
 public:
-    Direccion();
-    string obtenerCalle();
-    string obtenerCiudad();
-    string obtenerCodigoPostal();
-    string obtenerEstado();
-    string obtenerPais();
-    void modificarCalle(string calle);
-    void modificarCiudad(string ciudad);
-    void modificarCodigoPostal(string codigoPostal);
-    void modificarEstado(string estado);
-    void modificarPais(string pais);
+    Direccion(); // Constructor de la clase Direccion.
+    string obtenerCalle(); // Método para obtener el nombre de la calle.
+    string obtenerCiudad(); // Método para obtener el nombre de la ciudad.
+    string obtenerCodigoPostal(); // Método para obtener el código postal.
+    string obtenerEstado(); // Método para obtener el estado.
+    string obtenerPais(); // Método para obtener el nombre del país.
+    void modificarCalle(string calle); // Método para establecer  el nombre de la calle.
+    void modificarCiudad(string ciudad); // Método para establecer  el nombre de la ciudad.
+    void modificarCodigoPostal(string codigoPostal); // Método para establecer  el código postal.
+    void modificarEstado(string estado); // Método para establecer  el estado o provincia.
+    void modificarPais(string pais); // Método para establecer  el nombre del país.
 };
 
 #endif
